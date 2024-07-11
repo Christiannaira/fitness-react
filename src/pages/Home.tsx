d8ce6d0a3634d9011da4947cedca8cbf79947058
Christian <Christianmnaira@gmail.com>
2024-07-11 11:02:08 +0800

    feat: Added a body css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,17 @@
-import { Box } from "@mui/material"
+import { Box } from "@mui/material";
+
+import Exercises from "../components/Exercises";
+import SearchExercises from "../components/SearchExercises";
+import HeroBanner from "../components/HeroBanner";
 
 
 const Home = () => {
   return (
 
     <Box>
-      hello world
+      <HeroBanner/>
+      <SearchExercises/>
+      <Exercises/>
     </Box>
 
   )
