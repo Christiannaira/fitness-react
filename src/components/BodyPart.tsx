0879b92b3654b8af1e7ff2196627e19c0f2cc219
Christian <Christianmnaira@gmail.com>
2024-07-12 11:40:08 +0800

    feat: Added an event in bodypart component

--- src/components/BodyPart.tsx
+++ src/components/BodyPart.tsx
@@ -10,6 +10,10 @@ const BodyPart = ({item, setBodyPart, bodyPart}) => {
     className="bodyPart-card"
     sx={bodyPart === item? {
         borderTop: '4px solid #FF2625', background: '#fff', borderBottomLeftRadius: '20px', width: '270px', height: '282px', cursor: 'pointer', gap: '47px'} : {background: '#fff', borderBottomLeftRadius: '20px', width: '270px', height: '282[x', cursor: 'pointer', gap: '47px'}}
+    onClick={() => {
+        setBodyPart(item);
+        window.scrollTo({top: 1800, left: 100, behavior: 'smooth'});
+    }}
     >
       
         <img src={Icon} alt="dumbbell" style={{width: '40px', height: '40px'}}/>
--- src/components/HorizontalScrollbar.tsx
+++ src/components/HorizontalScrollbar.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Box } from '@mui/material';
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 
+import RightArrowIcon from '../assets/icons/right-arrow.png';
+import LeftArrowIcon from '../assets/icons/left-arrow.png';
 import BodyPart from './BodyPart';
 
 const HorizontalScrollbar = ({data, bodyPart, setBodyPart}) => {
