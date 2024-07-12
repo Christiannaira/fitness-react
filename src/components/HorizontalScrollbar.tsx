98641be0a83256bfc144fc33ec292e45ba57cb95
Christian <Christianmnaira@gmail.com>
2024-07-12 12:05:03 +0800

    feat: Added a functionality in horizontal scrollbar component

--- src/components/HorizontalScrollbar.tsx
+++ src/components/HorizontalScrollbar.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Box } from '@mui/material';
+import React, { useContext } from 'react';
+import { Box, Typography } from '@mui/material';
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 import 'react-horizontal-scrolling-menu/styles.css'; // import react-horizontal-scrolling-menu
 
@@ -7,12 +7,38 @@ import RightArrowIcon from '../assets/icons/right-arrow.png';
 import LeftArrowIcon from '../assets/icons/left-arrow.png';
 import BodyPart from './BodyPart';
 
+
+const LeftArrow = () => {
+
+    const {scorllPrev} = useContext(VisibilityContext);
+
+    return (
+        <Typography onClick={() => scorllPrev()} className="right-arrow">
+            <img src={LeftArrowIcon} alt="right-arrow" />
+        </Typography>
+    )
+
+}
+
+const RightArrow = () => {
+
+    const {scorllNext} = useContext(VisibilityContext);
+
+    return (
+        <Typography onClick={() => scorllNext()} className="left-arrow">
+            <img src={RightArrowIcon} alt="left-arrow" />
+        </Typography>
+    )
+
+}
+
+
 const HorizontalScrollbar = ({data, bodyPart, setBodyPart}) => {
 
 
   return (
     <div>
-        <ScrollMenu>
+        <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
 
         {data.map((item) => (
 
