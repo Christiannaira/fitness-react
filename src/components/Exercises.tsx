84e1bbf86f68662b1fba8f6c5e8a603c2d88a6ee
Christian <Christianmnaira@gmail.com>
2024-07-16 17:10:04 +0800

    fix: Fixed the typo

--- src/components/Exercises.tsx
+++ src/components/Exercises.tsx
@@ -48,7 +48,7 @@ const Exercises = ({exercises, setExercises, bodyPart}) => {
 
 
   return (
-    <Box id="exercsies" sx={{mt: {lg: '110px'}}} mt="50px" p="20px">
+    <Box id="exercises" sx={{mt: {lg: '110px'}}} mt="50px" p="20px">
 
     <Typography variant="h3" mb="46px">
       Showing Results
